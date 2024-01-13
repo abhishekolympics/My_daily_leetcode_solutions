393ae900b304b847f1c9ebe16539309c57fe8268
Abhishek Kumar Singh <abhishek.kumar@sifars.com>
2024-01-13 13:49:52 +0530

    Day 2

---
+++ zero one Knapsack.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       vector<vector<int>> vec(n+1,vector<int>(W+1,0));
+       for(int i=1;i<n+1;i++)
+       {
+           for(int j=1;j<W+1;j++)
+           {
+               if(wt[i-1]<=j)
+               {
+                   vec[i][j]=max(val[i-1]+vec[i-1][j-wt[i-1]],vec[i-1][j]);
+               }
+               else
+               {
+                   vec[i][j]=vec[i-1][j];
+               }
+           }
+       }
+       return vec[n][W];
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
