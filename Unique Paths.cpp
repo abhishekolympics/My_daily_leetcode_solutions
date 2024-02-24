cb9c081b1f2bd0dce77d244624894997b821d65e
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-02-24 12:05:48 +0530

    Unique Paths

---
+++ Unique Paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return helper(m-1,n-1,dp);
+    }
+    int helper(int m,int n,vector<vector<int>>& dp)
+    {
+        if(m<0 || n<0)
+        {
+            return 0;
+        }
+        if(dp[m][n]!=-1)
+        {
+            return dp[m][n];
+        }
+        if(m==0 ||n==0)
+        {
+            return 1;
+        }
+        return dp[m][n]=(helper(m-1,n,dp)+helper(m,n-1,dp));
+    }
+};
