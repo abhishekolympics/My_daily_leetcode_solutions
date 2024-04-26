d29ae5ca889d918295dc6f32a346d5ab5932acd7
Abhishek Kumar Singh <abhishekolympics@gmail.com>
2024-04-26 22:23:43 +0530

    Spiral matrix

---
+++ 2nd revision/Spiral matrix .cpp
@@ -0,0 +1,41 @@
+//watch https://www.youtube.com/watch?v=3Zv-s9UUrFM
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        int left=0,top=0,bottom=m-1,right=n-1;
+        vector<int>ans;
+        while(top<=bottom && left<=right)
+        {
+            for(int i=left;i<=right;i++)
+            {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int i=top;i<=bottom;i++)
+            {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            if(top<=bottom)
+            {
+
+                for(int i=right;i>=left;i--)
+                {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if(left<=right)
+            {
+                for(int i=bottom;i>=top;i--)
+                {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
